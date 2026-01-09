e7db09390aa71602ec850a0298753bcf7a8025d4
aysswarya-1 <a.aysswarya@gmail.com>
2026-01-09 12:28:00 +0530

    Added Paths

--- server.js
+++ server.js
@@ -8,21 +8,26 @@ import authRoutes from './routes/auth.routes.js'
 import incomeRoutes from './routes/income.routes.js'
 import expenseRoutes from './routes/expense.routes.js'
 import dashboardRoutes from './routes/dashboard.routes.js'
-import { fileURLToPath } from 'url';
+
+// Load env based on environment
+dotenv.config({
+    path: `.env.${process.env.NODE_ENV || "development"}`
+});
+
 const app = express()
-dotenv.config()
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json())
+app.use(cookieParser());
 
 app.use(
     cors({
         origin: process.env.CLIENT_URL,
+        credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
-
-app.use(express.json())
-app.use(cookieParser());
 
 app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/income', incomeRoutes)
